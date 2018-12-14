8b5b4dd702a9ec0cc6bf62ba9d867e7116b0a9d8
Yuri Maeda <s1f101802601@iniad.org>
2018-12-14 15:37:57 +0900

    設定画面のhtml

--- table/views.py
+++ table/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def table(request):
     """時間割画面"""
     return render(request, 'timetable.html')
+
+def setting(request):
+    """時間割画面"""
+    return render(request, 'setting.html')
