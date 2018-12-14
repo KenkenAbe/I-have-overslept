63ab42dda8aaec0d22a3b59531d46c1bdf1c86e1
Kentaro <30878285+KenkenAbe@users.noreply.github.com>
2018-12-14 16:01:31 +0900

    ログインページの作成
    ・Googleのログインページへリダイレクトするところまで

--- table/views.py
+++ table/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def table(request):
@@ -8,3 +8,8 @@ def table(request):
 def setting(request):
     """時間割画面"""
     return render(request, 'setting.html')
+
+def login(request):
+    """Googleへのログイン（そのままリダイレクト）"""
+
+    return redirect("https://accounts.google.com/o/oauth2/auth?client_id=428921365707-kmk7u4ns6djre3lqa4k8mbdopeb41742.apps.googleusercontent.com&redirect_uri=https://scheduler.iniadstulab.jp/login/g/callback&scope=openid%20email%20profile&response_type=code&hd=iniad.org")
