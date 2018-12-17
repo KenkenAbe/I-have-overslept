fc4cb942cf817bd8ef1feea47aca3231a5837ac5
Yuri Maeda <s1f101802601@iniad.org>
2018-12-17 17:13:07 +0900

    データ取得
    うーん、使い方あってる？？

--- table/static/script/table.js
+++ table/static/script/table.js
@@ -1,10 +1,13 @@
+var table=timetables.objects.all;
+
 //var g1=document.getElementById(g1); 
-//g1.textContent=文字列; 
+//g1.textContent=table; 
 //var k1=document.getElementById(k1); 
-//k1.textContent=文字列; 
+//k1.textContent=table; 
 //var s1=document.getElementById(s1); 
-//s1.textContent=文字列; 
+//s1.textContent=table; 
 //var m1=document.getElementById(m1); 
-//m1.textContent=文字列; 
+//m1.textContent=table; 
 //var i1=document.getElementById(i1); 
-//i1.textContent=文字列; 
+//i1.textContent=table; 
+
