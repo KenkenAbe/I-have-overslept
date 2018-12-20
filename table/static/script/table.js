4e3e2804781a151c0dbcd0cd042e9c0176258217
Kentaro <30878285+KenkenAbe@users.noreply.github.com>
2018-12-20 18:47:45 +0900

    フォーム仮
    科目名を手動入力して、時間と曜日を自動取得する

--- table/static/script/table.js
+++ table/static/script/table.js
@@ -11,6 +11,6 @@
 
 function rewriteFormModal(week,time){
     let weeks = ["月","火","水","木","金"]
-    document.getElementById("form-week").innerText = "曜日　："+weeks[week]+"曜日";
-    document.getElementById("form-time").innerText = "時間　："+String(time)+"時限";
+    document.getElementById("form-week").value = weeks[week];
+    document.getElementById("form-time").value = String(time);
 }
